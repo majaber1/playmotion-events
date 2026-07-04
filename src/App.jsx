ed4d43aac0e3978603a127316a3451ee602d786b
majaber1 <jaber.a.moh@outlook.com>
2026-07-05 00:59:55 +0300

    Update App.jsx routes for new site structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,17 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Navbar from './components/Navbar'
 import Footer from './components/Footer'
+import WhatsAppButton from './components/WhatsAppButton'
 import Home from './pages/Home'
 import About from './pages/About'
-import MissionVision from './pages/MissionVision'
 import Products from './pages/Products'
-import ProductDetail from './pages/ProductDetail'
-import Categories from './pages/Categories'
+import MultiBall from './pages/MultiBall'
+import XWall from './pages/XWall'
+import SoccerTable from './pages/SoccerTable'
+import EventSolutions from './pages/EventSolutions'
 import Gallery from './pages/Gallery'
-import Events from './pages/Events'
-import Packages from './pages/Packages'
+import Videos from './pages/Videos'
+import Booking from './pages/Booking'
 import Contact from './pages/Contact'
 
 function App() {
@@ -21,17 +23,19 @@ function App() {
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/about" element={<About />} />
-            <Route path="/mission-vision" element={<MissionVision />} />
             <Route path="/products" element={<Products />} />
-            <Route path="/products/:id" element={<ProductDetail />} />
-            <Route path="/categories" element={<Categories />} />
+            <Route path="/multiball" element={<MultiBall />} />
+            <Route path="/x-wall" element={<XWall />} />
+            <Route path="/soccer-table" element={<SoccerTable />} />
+            <Route path="/event-solutions" element={<EventSolutions />} />
             <Route path="/gallery" element={<Gallery />} />
-            <Route path="/events" element={<Events />} />
-            <Route path="/packages" element={<Packages />} />
+            <Route path="/videos" element={<Videos />} />
+            <Route path="/booking" element={<Booking />} />
             <Route path="/contact" element={<Contact />} />
           </Routes>
         </main>
         <Footer />
+        <WhatsAppButton />
       </div>
     </Router>
   )
