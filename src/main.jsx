3f409d8a15ee1f2a3aae187d74979051e35e7da9
majaber1 <jaber.a.moh@outlook.com>
2026-07-18 16:04:24 +0300

    Wrap App with LanguageProvider in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,1 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { HelmetProvider } from 'react-helmet-async'
-import App from './App.jsx'
-import './index.css'
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <HelmetProvider>
-      <App />
-    </HelmetProvider>
-  </React.StrictMode>,
-)
+import React from 'react'; import ReactDOM from 'react-dom/client'; import { HelmetProvider } from 'react-helmet-async'; import App from './App.jsx'; import { LanguageProvider } from './context/LanguageContext'; import './index.css'; ReactDOM.createRoot(document.getElementById('root')).render(React.createElement(React.StrictMode, null, React.createElement(HelmetProvider, null, React.createElement(LanguageProvider, null, React.createElement(App)))));
